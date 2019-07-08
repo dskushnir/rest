7717adb3f237e08b2c6ceddb5fd1d9c1a022a047
Diana <kpds7175@gmail.com>
2019-07-08 22:21:41 +0300

    create project

---
+++ src/main/java/hillel/mySpringRest/MySpringRestApplication.java
@@ -0,0 +1,13 @@
+package hillel.mySpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringRestApplication.class, args);
+	}
+
+}
